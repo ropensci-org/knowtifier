363ec20a821d01c3ed5b6bf97f381e062f11724d
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-13 07:42:06 -0700

    #3 changes to how tweets are made - using title instead of description in DESCRIPTION file remove a few dots and such to save space

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
-ruby '2.3.2'
+ruby '2.4.1'
 
-gem 'rake', '~> 10.4.2'
-gem 'faraday', '~> 0.9.0'
-gem 'multi_json', '~> 1.12', '>= 1.12.1'
-gem 'twitter', '~> 5.16'
+gem 'rake', '~> 12.1.0'
+gem 'faraday', '~> 0.11.0'
+gem 'multi_json', '~> 1.12', '>= 1.12.2'
+gem 'twitter', '~> 6.1.0'
--- knowtifier.rb
+++ knowtifier.rb
@@ -19,6 +19,19 @@ module Knowtifier
     y[0..len]
   end
 
+  def self.clean_desc2(y)
+    y = y.gsub(/\n/, ' ')
+    y.match(/^.{0,140}\b/)[0]
+  end
+
+  def smart_truncate(x, len = 135, suffix = '...')
+    if x.length <= len
+      return x
+    else
+      return x[0..len + 1].split(' ')[0..-1].join(' ') + suffix
+    end
+  end
+
   def self.check(package, time_period)
     puts 'checking package "' + package + '"'
 
@@ -42,10 +55,10 @@ module Knowtifier
         new_pkg_date.utc
 
         if time_since(new_pkg_date) <= time_period
-          tweet = "New @rOpenSci pkg. %s (v.%s) https://cran.r-project.org/web/packages/%s" % [
+          tweet = "New @rOpenSci pkg. %s (v%s) https://cran.r-project.org/package=%s" % [
             package, ver1, package
           ]
-          tweet = tweet + ' ' + clean_desc(tweet.length, res['versions'][ver1]['Description'])
+          tweet = clean_desc2(tweet + ' ' + res['versions'][ver1]['Title'])
 
           # if tweet already sent, skip
           mytweets = $twitclient.user_timeline
@@ -72,10 +85,10 @@ module Knowtifier
         new_ver_date.utc
 
         if time_since(new_ver_date) <= time_period
-          tweet = "New ver. @rOpenSci pkg. %s (v.%s) https://cran.r-project.org/web/packages/%s" % [
+          tweet = "New ver @rOpenSci pkg %s (v%s) https://cran.r-project.org/package=%s -" % [
             package, ver1, package
           ]
-          tweet = tweet + ' ' + clean_desc(tweet.length, res['versions'][ver1]['Description'])
+          tweet = clean_desc2(tweet + ' ' + res['versions'][ver1]['Title'])
 
           # if tweet already sent, skip
           mytweets = $twitclient.user_timeline
@@ -94,6 +107,52 @@ module Knowtifier
       end
     end
   end
+
+  # def self.calculate(package, time_period)
+  #   puts 'checking package "' + package + '"'
+
+  #   cn = Faraday.new(:url => 'http://crandb.r-pkg.org/%s/all' % package) do |f|
+  #     f.adapter Faraday.default_adapter
+  #   end
+  #   z = cn.get
+  #   if !z.success?
+  #     puts 'skipping %s - not on CRAN' % package
+  #   else
+  #     res = MultiJson.load(z.body)
+  #     ver1 = res['versions'].keys.last
+
+  #     # check if a new package
+  #     if res['timeline'].length > 1
+  #       new_pkg = false
+  #     else
+  #       new_pkg = true
+  #       new_pkg_date = res['timeline'][ver1]
+  #       new_pkg_date = Time.parse(new_pkg_date.sub('+00:00', '+01:00'))
+  #       new_pkg_date.utc
+
+  #       tweet = "New @rOpenSci pkg. %s (v%s) https://cran.r-project.org/package=%s -" % [
+  #         package, ver1, package
+  #       ]
+  #       tweet = clean_desc2(tweet + ' ' + res['versions'][ver1]['Title'])
+  #       return tweet
+  #     end
+
+  #     # check if a new version
+  #     if new_pkg
+  #       puts 'new package, skipping new version check'
+  #     else
+  #       new_ver_date = res['timeline'][ver1]
+  #       new_ver_date = Time.parse(new_ver_date.sub('+00:00', '+01:00'))
+  #       new_ver_date.utc
+
+  #       tweet = "New ver @rOpenSci pkg %s (v%s) https://cran.r-project.org/package=%s -" % [
+  #         package, ver1, package
+  #       ]
+  #       tweet = clean_desc2(tweet + ' ' + res['versions'][ver1]['Title'])
+  #       return tweet
+  #     end
+  #   end
+  # end
 
   def self.get_packages
     conn = Faraday.new(:url => 'https://raw.githubusercontent.com/ropensci/roregistry/master/registry.json') do |f|
